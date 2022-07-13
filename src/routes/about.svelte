1d1f14f48614ab2694ab060c79c1dd2e6f577fac
rockvan <dev.rockvan@gmail.com>
2022-07-13 18:40:06 +0800

    initial

---
+++ src/routes/about.svelte
@@ -0,0 +1,23 @@
+<script>
+</script>
+
+<div class="about">
+  <h2>About Page</h2>
+  <p>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda,
+    similique voluptates cupiditate beatae obcaecati rerum facere, quod quia
+    neque odio natus magni ex aliquid dolorum temporibus cum consequatur earum
+    ad.
+  </p>
+
+  <a href="/guides">View Guides</a>
+  <a href="/">Home</a>
+</div>
+
+<style>
+  .about {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,30 @@
+<script>
+</script>
+
+<div class="index">
+  <h2>Home Page</h2>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem doloremque et
+    quibusdam eaque officiis nisi fugit similique vel debitis, fugiat temporibus
+    impedit at voluptate omnis. Adipisci tenetur suscipit id vero corporis!
+    Inventore neque cupiditate voluptatibus dignissimos hic fugit aliquam sit
+    reprehenderit ea repellendus sapiente maxime natus ipsa aliquid modi
+    officia, cumque earum tempora saepe sint id. Voluptatem pariatur optio
+    dignissimos. Vero praesentium, dicta aut, sint omnis labore fugiat in
+    laboriosam, id corporis ab reiciendis nostrum quae! Ex, totam veniam.
+    Asperiores reiciendis maxime dolorem, rem minima dicta sit, nihil
+    repellendus velit aperiam cum blanditiis, facere distinctio. Optio
+    doloremque nemo voluptatibus tempora?
+  </p>
+
+  <a href="/guides">View Guides</a>
+  <a href="/about">About</a>
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
